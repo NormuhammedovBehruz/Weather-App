16e5704142704d5d3731da96eabf1372221f8ccd
NurmuhammadovBehruz <b.nurmuhammadov13@gmail.com>
2022-04-21 10:14:21 +0500

    Weather App

---
+++ app.js
@@ -0,0 +1,31 @@
+const searchForm = document.querySelector('.search-form')
+const search = document.querySelector('.search')
+const cityName = document.querySelector('.city-name')
+const currentDegree = document.querySelector('.current-degree')
+const currentWeather = document.querySelector('.current-weather')
+const min = document.querySelector('.min')
+const max = document.querySelector('.max')
+
+const api = {
+    key: '467143a2e9e1de3de89328bf80dd0625',
+    base: 'https://api.openweathermap.org/data/2.5/',
+}
+
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const nameCity = search.value
+    fetch(`${api.base}weather?q=${nameCity}&units=metric&appid=${api.key}`)
+     .then((data) => {
+         return data.json()
+     })
+     .then(getWeather)
+})
+
+function getWeather(data) {
+    cityName.textContent = `${data.name}, ${data.sys.country}`
+    currentDegree.textContent = `${Math.round(data.main.temp)}℃`
+    currentWeather.textContent = `${data.weather[0].main}`
+    min.textContent = `${Math.round(data.main.temp_min)}℃`
+    max.textContent = `${Math.round(data.main.temp_max)}℃`
+    search.value = ''
+}
